cb040485b9a34448d3005544e79f12869be3f0c9
LF-netizen <lukaszfaruga@student.agh.edu.pl>
2023-01-13 16:03:31 +0100

    tests updated

--- src/test_100x_repeat.py
+++ src/test_100x_repeat.py
@@ -15,24 +15,32 @@ epoch_size = 50
 nbhoods_active = [1, 1, 1, 1, 1, 1]
 ###
 
-for it_model, model_loader in enumerate([dense_model_predefined, sparse_model_predefined, mean_model_predefined]):
-    adjmatrix, arch, schedule = model_loader()
-    network = Network(arch)
-    network.load_schedule(schedule)
-    model = OptimizationModel(network, adjmatrix)
+with open('logs/test_100x_repeat.txt', 'w') as file:
+    for it_model, model_loader in enumerate([dense_model_predefined, sparse_model_predefined, mean_model_predefined]):
+        t = time.localtime()
+        current_time = time.strftime("%H:%M:%S", t)
+        print(f'================\nMODEL {it_model}: {current_time} CET')
+        
+        file.write(f'MODEL {it_model}\n')
 
-    nbhoods = [model.change_solution, model.change_solution2, model.change_solution3, model.change_solution4, model.change_solution5, model.change_solution6]
-    nbhoods = set(compress(nbhoods, nbhoods_active))
+        adjmatrix, arch, schedule = model_loader()
+        network = Network(arch)
+        network.load_schedule(schedule)
+        model = OptimizationModel(network, adjmatrix)
 
-    for iter in range(iterations):
-        time_start = time.perf_counter()
-        _, _, cost_array = model.run_model(t0, t1, alpha, epoch_size, neighbourhoods_active=nbhoods)
-        time_stop = time.perf_counter()
-        print(len(cost_array))
-        model.network.reset_state(with_schedule=False)
-        print(f'{(time_stop - time_start)*1000:.0f}')
-    
-    model.network.reset_state(with_devices=True)
+        nbhoods = [model.change_solution, model.change_solution2, model.change_solution3, model.change_solution4, model.change_solution5, model.change_solution6]
+        nbhoods = set(compress(nbhoods, nbhoods_active))
+
+        for iter in range(iterations):
+            print(f'Iter {iter}')
+            time_start = time.perf_counter()
+            _, _, cost_array = model.run_model(t0, t1, alpha, epoch_size, neighbourhoods_active=nbhoods)
+            time_stop = time.perf_counter()
+            
+            file.write(f'{(time_stop - time_start):.3f} s; {cost_array}\n')
+            
+            model.network.reset_state(with_schedule=False)
+        model.network.reset_state(with_devices=True)
     
 
         
