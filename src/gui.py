bb9ed8910c5dc5ebb179e39dc331e2c29d6d31aa
LF-netizen <lukaszfaruga@student.agh.edu.pl>
2023-01-17 17:42:29 +0100

    routing table as table

--- src/gui.py
+++ src/gui.py
@@ -261,110 +261,82 @@ class RoutingTable(customtkinter.CTkFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)  
+        # self.grid_rowconfigure(0, weight=1)
+        # self.grid_columnconfigure(0, weight=1)  
 
-        self.textbox = customtkinter.CTkTextbox(self, width=500, height=500, activate_scrollbars=False, font=customtkinter.CTkFont(size=14))
-        self.textbox.grid(row=0, column=0)
+        # self.textbox = customtkinter.CTkTextbox(self, width=500, height=500, activate_scrollbars=False)
+        # self.textbox.grid(row=0, column=0)
+
+        self.canvas = tk.Canvas(self, width=500, height=500)
+        self.canvas.grid(row=0, column=0, padx=20, pady=(0, 20))
+
+        self.fig = Figure(figsize=(9, 6))
+        self.figure_canvas = FigureCanvasTkAgg(self.fig, self.canvas)
+        self.figure_canvas.draw()
+        self.figure_canvas.get_tk_widget().pack(side='top', fill='both', expand=1)
+
 
     def write_to_textbox(self, string):
         self.textbox.insert('0.0', string)
+    
+    def write_to_table(self, string):
+        self.fig.clear()
+        ax = self.fig.add_subplot(111)
 
-# class RoutingTable(customtkinter.CTkFrame):
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
+        title_text = 'Routing tables'
 
-#         self.grid_rowconfigure(0, weight=1)
-#         self.grid_columnconfigure(0, weight=1)  
+        data =  [
+                    [         'Freeze', 'Wind', 'Flood', 'Quake', 'Hail'],
+                    [ '5 year',  66386, 174296,   75131,  577908,  32015],
+                    ['10 year',  58230, 381139,   78045,   99308, 160454],
+                    ['20 year',  89135,  80552,  152558,  497981, 603535],
+                    ['30 year',  78415,  81858,  150656,  193263,  69638],
+                    ['40 year', 139361, 331509,  343164,  781380,  52269],
+                ]
+        # column_headers = data.pop(0)
+        # row_headers = [x.pop(0) for x in data]
 
-#         self.textbox = customtkinter.CTkTextbox(self, width=500, height=500, activate_scrollbars=False)
-#         self.textbox.grid(row=0, column=0)
-
-#         self.canvas = tk.Canvas(self, width=500, height=500)
-#         self.canvas.grid(row=0, column=0, padx=20, pady=(0, 20))
+        solution = wrapper.model.solution
+        row_headers = list(solution.keys())
+        column_headers = list(solution[row_headers[0]].keys())
         
-#         fig_background_color = 'skyblue'
-#         fig_border = 'steelblue'
-#         self.fig = Figure(figsize=(6, 6), linewidth=2,
-#                 edgecolor=fig_border,
-#                 facecolor=fig_background_color,
-#                 tight_layout={'pad':1})
-#         self.figure_canvas = FigureCanvasTkAgg(self.fig, self.canvas)
-#         self.figure_canvas.draw()
-#         self.figure_canvas.get_tk_widget().pack(side='top', fill='both', expand=1)
-    
-#     def write_to_textbox(self, string):
-#         self.textbox.insert('0.0', string)
-    
-#     def plot_table(self):
-#         ax = self.fig.add_subplot(111)
+        
+        data = [[solution[r][c] for c in column_headers] for r in row_headers]
 
-#         title_text = 'Loss by Disaster'
-#         footer_text = 'June 24, 2020'
+        print(data)
+        print(row_headers)
+        print(column_headers)
+        print(solution)
+        
+        cell_text = []
+        for row in data:
+            cell_text.append([f'{x}' for x in row])
+        # Get some lists of color specs for row and column headers
+        rcolors = plt.cm.BuPu(np.full(len(row_headers), 0.1))
+        ccolors = plt.cm.BuPu(np.full(len(column_headers), 0.1))
+        # Create the figure. Setting a small pad on tight_layout
+        # seems to better regulate white space. Sometimes experimenting
+        # with an explicit figsize here can produce better outcome.
+        # plt.figure(linewidth=2,
+        #         )
+        # Add a table at the bottom of the axes
+        
+        ax.table(cellText=cell_text,
+                            rowLabels=row_headers,
+                            rowColours=rcolors,
+                            rowLoc='right',
+                            colColours=ccolors,
+                            colLabels=column_headers,
+                            loc='center')
 
-#         data =  [
-#                     [         'Freeze', 'Wind', 'Flood', 'Quake', 'Hail'],
-#                     [ '5 year',  66386, 174296,   75131,  577908,  32015],
-#                     ['10 year',  58230, 381139,   78045,   99308, 160454],
-#                     ['20 year',  89135,  80552,  152558,  497981, 603535],
-#                     ['30 year',  78415,  81858,  150656,  193263,  69638],
-#                     ['40 year', 139361, 331509,  343164,  781380,  52269],
-#                 ]
-#         # Pop the headers from the data array
-#         column_headers = data.pop(0)
-#         row_headers = [x.pop(0) for x in data]
-#         # Table data needs to be non-numeric text. Format the data
-#         # while I'm at it.
-#         cell_text = []
-#         for row in data:
-#             cell_text.append([f'{x/1000:1.1f}' for x in row])
-#         # Get some lists of color specs for row and column headers
-#         rcolors = plt.cm.BuPu(np.full(len(row_headers), 0.1))
-#         ccolors = plt.cm.BuPu(np.full(len(column_headers), 0.1))
-#         # Create the figure. Setting a small pad on tight_layout
-#         # seems to better regulate white space. Sometimes experimenting
-#         # with an explicit figsize here can produce better outcome.
-#         # plt.figure(linewidth=2,
-#         #         edgecolor=fig_border,
-#         #         facecolor=fig_background_color,
-#         #         tight_layout={'pad':1},
-#         #         #figsize=(5,3)
-#         #         )
-#         # Add a table at the bottom of the axes
-#         the_table = plt.table(cellText=cell_text,
-#                             rowLabels=row_headers,
-#                             rowColours=rcolors,
-#                             rowLoc='right',
-#                             colColours=ccolors,
-#                             colLabels=column_headers,
-#                             loc='center')
-#         # Scaling is the only influence we have over top and bottom cell padding.
-#         # Make the rows taller (i.e., make cell y scale larger).
-#         the_table.scale(1, 1.5)
-#         # Hide axes
-#         # ax = plt.gca()
-#         # ax.get_xaxis().set_visible(False)
-#         # ax.get_yaxis().set_visible(False)
-#         # Hide axes border
-#         plt.box(on=None)
-#         # Add title
-#         plt.suptitle(title_text)
-#         # Add footer
-#         plt.figtext(0.95, 0.05, footer_text, horizontalalignment='right', size=6, weight='light')
-#         # Force the figure to update, so backends center objects correctly within the figure.
-#         # Without plt.draw() here, the title will center on the axes and not the figure.
-#         # plt.draw()
-#         # Create image. plt.savefig ignores figure edge and face colors, so map them.
-#         ax.plot()
-#         self.fig.canvas.draw()
-#         # fig = plt.gcf()
-#         # plt.show()
-#         # plt.savefig('pyplot-table-demo.png',
-#         #             #bbox='tight',
-#         #             edgecolor=fig.get_edgecolor(),
-#         #             facecolor=fig.get_facecolor(),
-#         #             dpi=150
-#         #             )
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        self.fig.suptitle(title_text)
+      
+        self.fig.canvas.draw()
+
+
 
 
 
@@ -724,8 +696,10 @@ class ChartFrame(customtkinter.CTkFrame):
     def routing_table_button_event(self):
         self.select_frame_by_button(self.routing_table_button)
 
-        self.routing_table.write_to_textbox(wrapper.model.solution)
+        self.routing_table.write_to_table(wrapper.model.solution)
         # self.routing_table.plot_table()
+
+    
 
 
     def chart_button_event(self):
